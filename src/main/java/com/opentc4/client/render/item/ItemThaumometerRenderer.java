b06eae5b23ed0d3ccb7fd1e846422d0512bfbc8b
wrldmap <123031534+wrldmap@users.noreply.github.com>
2025-06-26 06:16:00 -0500

    yeahhhh
    it works if you use a resource pack with the original model, still gotta get a model and texture that works

--- src/main/java/com/opentc4/client/render/item/ItemThaumometerRenderer.java
+++ src/main/java/com/opentc4/client/render/item/ItemThaumometerRenderer.java
@@ -1,17 +1,25 @@
 package com.opentc4.client.render.item;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
+import java.lang.reflect.Field;
 
 public class ItemThaumometerRenderer implements IItemRenderer {
 
@@ -38,24 +46,66 @@ public class ItemThaumometerRenderer implements IItemRenderer {
         return true; // Allow helpers for smooth rendering
     }
 
-    private void renderQuad(float size, float alpha) {
-        Tessellator tessellator = Tessellator.instance;
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, alpha);
+    private float smoothEquipProgress = 1.0F;
+    private float smoothPitch = 0f;
+    private float smoothYaw = 0f;
 
+    private void updateSmoothRotation(float targetPitch, float targetYaw) {
+        float smoothing = 0.1f; // adjust for speed
+
+        smoothPitch += (targetPitch - smoothPitch) * smoothing;
+        smoothYaw += (targetYaw - smoothYaw) * smoothing;
+    }
+
+    private float getSmoothPitch() {
+        return smoothPitch;
+    }
+
+    private float getSmoothYaw() {
+        return smoothYaw;
+    }
+
+    private void updateEquipProgress(float targetProgress) {
+        smoothEquipProgress += (targetProgress - smoothEquipProgress) * 0.1F; // adjust smoothing speed here
+    }
+
+    private float getSmoothEquipProgress() {
+        return smoothEquipProgress;
+    }
+
+    private void renderQuadCenteredFromTexture(Minecraft mc, ResourceLocation texture, float scale, float r, float g, float b, int alpha, int blendMode) {
+        GL11.glPushMatrix();
+        GL11.glScalef(scale, scale, scale);
+        mc.getTextureManager().bindTexture(texture);
+
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, blendMode);
+        GL11.glColor4f(r, g, b, alpha / 255.0F);
+
+        Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
-        tessellator.addVertexWithUV(-size, size, 0, 0, 1);
-        tessellator.addVertexWithUV(size, size, 0, 1, 1);
-        tessellator.addVertexWithUV(size, -size, 0, 1, 0);
-        tessellator.addVertexWithUV(-size, -size, 0, 0, 0);
+        tessellator.addVertexWithUV(-0.5, 0, -0.5, 0, 0);
+        tessellator.addVertexWithUV(0.5, 0, -0.5, 1, 0);
+        tessellator.addVertexWithUV(0.5, 0, 0.5, 1, 1);
+        tessellator.addVertexWithUV(-0.5, 0, 0.5, 0, 1);
         tessellator.draw();
 
-        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glColor4f(1, 1, 1, 1);
+        GL11.glPopMatrix();
     }
 
     @Override
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
         Minecraft mc = Minecraft.getMinecraft();
+        int renderedViewEntityId = 0;
+        int playerEntityId = 0;
+
+        if (type == ItemRenderType.EQUIPPED) {
+            renderedViewEntityId = mc.renderViewEntity.getEntityId();
+            playerEntityId = ((EntityLivingBase) data[1]).getEntityId();
+        }
+
         EntityPlayer player = mc.thePlayer;
 
         float partialTicks = 0F;
@@ -63,87 +113,110 @@ public class ItemThaumometerRenderer implements IItemRenderer {
             partialTicks = (Float) data[1];
         }
 
+        float scaleBase = 0.8F;
+        EntityPlayerSP playerSP = (EntityPlayerSP) player;
+        float equippedProgress = 1.0F;
+        float prevEquippedProgress = 1.0F;
+
+        try {
+            Field equippedField = ItemRenderer.class.getDeclaredField("equippedProgress");
+            Field prevEquippedField = ItemRenderer.class.getDeclaredField("prevEquippedProgress");
+            equippedField.setAccessible(true);
+            prevEquippedField.setAccessible(true);
+
+            equippedProgress = equippedField.getFloat(mc.entityRenderer.itemRenderer);
+            prevEquippedProgress = prevEquippedField.getFloat(mc.entityRenderer.itemRenderer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        float equipProgressInterpolated = prevEquippedProgress + (equippedProgress - prevEquippedProgress) * partialTicks;
+        updateEquipProgress(equipProgressInterpolated);
+        float smoothProgress = getSmoothEquipProgress();
+        float pitchInterpolated = playerSP.prevRenderArmPitch + (playerSP.renderArmPitch - playerSP.prevRenderArmPitch) * partialTicks;
+        float yawInterpolated = playerSP.prevRenderArmYaw + (playerSP.renderArmYaw - playerSP.prevRenderArmYaw) * partialTicks;
+        updateSmoothRotation(pitchInterpolated, yawInterpolated);
+        float smoothPitch = getSmoothPitch();
+        float smoothYaw = getSmoothYaw();
+
         GL11.glPushMatrix();
         mc.renderEngine.bindTexture(TEXTURE_LOCATION);
 
-        if (type == ItemRenderType.EQUIPPED_FIRST_PERSON) {
-            RenderPlayer renderPlayer = (RenderPlayer) RenderManager.instance.getEntityRenderObject(mc.thePlayer);
+        if (type == ItemRenderType.EQUIPPED_FIRST_PERSON && playerEntityId == renderedViewEntityId && mc.gameSettings.thirdPersonView == 0) {
+            GL11.glTranslatef(1F, 0.75F, -1F);
+            GL11.glRotatef(135.0F, 0.0F, -1.0F, 0.0F);
 
+
+            GL11.glRotatef((player.rotationPitch - smoothPitch) * 0.1F, 1.0F, 0.0F, 0.0F);
+            GL11.glRotatef((player.rotationYaw - smoothYaw) * 0.1F, 0.0F, 1.0F, 0.0F);
+            GL11.glTranslatef(-0.7F * scaleBase, -(-0.65F * scaleBase) + (1.0F - smoothProgress) * 1.5F, 0.9F * scaleBase);
+            GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
+            GL11.glTranslatef(0.0F, 0.0F, -0.9F * scaleBase);
+            GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
+
+            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+
+            GL11.glPushMatrix();
+            GL11.glScalef(5.0F, 5.0F, 5.0F);
             mc.renderEngine.bindTexture(mc.thePlayer.getLocationSkin());
 
-            for (int side = 0; side < 2; side++) {
-                int direction = side * 2 - 1; // -1 for left, +1 for right
-
+            for (int i = 0; i < 2; i++) {
+                int sideMultiplier = i * 2 - 1;
                 GL11.glPushMatrix();
-
-                // Arm positioning
-                GL11.glTranslatef(0.0F, -0.6F, 1.1F * direction);
-                GL11.glRotatef(-45 * direction, 1.0F, 0.0F, 0.0F);
+                GL11.glTranslatef(0.0F, -0.6F, 1.1F * sideMultiplier);
+                GL11.glRotatef(-45 * sideMultiplier, 1.0F, 0.0F, 0.0F);
                 GL11.glRotatef(-90.0F, 0.0F, 0.0F, 1.0F);
                 GL11.glRotatef(59.0F, 0.0F, 0.0F, 1.0F);
-                GL11.glRotatef(-65 * direction, 0.0F, 1.0F, 0.0F);
+                GL11.glRotatef(-65 * sideMultiplier, 0.0F, 1.0F, 0.0F);
 
-                // Failsafe to make sure scale is correct
-                GL11.glScalef(1.0F, 1.0F, 1.0F);
-
-                // Render the arms with player's skin
-                renderPlayer.renderFirstPersonArm(mc.thePlayer);
+                Render render = RenderManager.instance.getEntityRenderObject(player);
+                if (render instanceof RenderPlayer) {
+                    ((RenderPlayer) render).renderFirstPersonArm(player);
+                }
 
                 GL11.glPopMatrix();
             }
 
-            // Basic positioning
-            GL11.glTranslatef(0.8F, 0.6F, -0.8F);
-            GL11.glRotatef(135.0F, 0.0F, -1.0F, 0.0F);
+            GL11.glPopMatrix();
 
-            // Smooth player head movement interpolation
-            float prevPitch = mc.thePlayer.prevRotationPitch;
-            float currPitch = mc.thePlayer.rotationPitch;
-            float prevYaw = mc.thePlayer.prevRotationYaw;
-            float currYaw = mc.thePlayer.rotationYaw;
+            GL11.glRotatef(90.0F, 0.0F, 0.0F, 1.0F);
+            GL11.glTranslatef(0.4F, -0.4F, 0.0F);
+            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+            GL11.glScalef(2.0F, 2.0F, 2.0F);
 
-            float interpPitch = prevPitch + (currPitch - prevPitch) * partialTicks;
-            float interpYaw = prevYaw + (currYaw - prevYaw) * partialTicks;
-
-            GL11.glRotatef((currPitch - interpPitch) * 0.1F, 1.0F, 0.0F, 0.0F);
-            GL11.glRotatef((currYaw - interpYaw) * 0.1F, 0.0F, 1.0F, 0.0F);
-
-            GL11.glScalef(1F, 1F, 1F);
-
-        } else if (type == ItemRenderType.EQUIPPED) {
-            GL11.glTranslatef(1.6F, 0.3F, 2.0F);
-            GL11.glRotatef(90.0F, -1.0F, 0.0F, 0.0F);
-            GL11.glRotatef(30.0F, 0.0F, 0.0F, -1.0F);
-            GL11.glScalef(1F, 1F, 1F);
-
-        } else if (type == ItemRenderType.INVENTORY) {
-            GL11.glRotatef(60.0F, 1.0F, 0.0F, 0.0F);
-            GL11.glRotatef(30.0F, 0.0F, 0.0F, -1.0F);
-            GL11.glRotatef(248.0F, 0.0F, -1.0F, 0.0F);
+        } else {
+            // Third person or inventory render
             GL11.glScalef(0.5F, 0.5F, 0.5F);
-
-        } else if (type == ItemRenderType.ENTITY) {
-            GL11.glScalef(0.5F, 0.5F, 0.5F);
+            if (type == ItemRenderType.EQUIPPED) {
+                GL11.glTranslatef(1.6F, 0.3F, 2.0F);
+                GL11.glRotatef(90.0F, -1.0F, 0.0F, 0.0F);
+                GL11.glRotatef(30.0F, 0.0F, 0.0F, -1.0F);
+            } else if (type == ItemRenderType.INVENTORY) {
+                GL11.glRotatef(60.0F, 1.0F, 0.0F, 0.0F);
+                GL11.glRotatef(30.0F, 0.0F, 0.0F, -1.0F);
+                GL11.glRotatef(248.0F, 0.0F, -1.0F, 0.0F);
+            } else if (type == ItemRenderType.ENTITY) {
+                GL11.glScalef(0.5F, 0.5F, 0.5F);
+            }
         }
 
-        model.renderAll();
+        mc.getTextureManager().bindTexture(new ResourceLocation("opentc4:textures/models/item/thaumometer.png"));
+        this.model.renderAll();
 
-        // scan screen :)
-        mc.renderEngine.bindTexture(new ResourceLocation("opentc4", "textures/models/item/scanscreen.png"));
+        // Render the screen quad
         GL11.glPushMatrix();
-        GL11.glScalef(0.75f, 0.75f, 0.75f);
-
-        if (type == ItemRenderType.INVENTORY) {
-            GL11.glScalef(0.75F, 0.75F, 0.75F);
-        }
-
         GL11.glTranslatef(0.0F, 0.11F, 0.0F);
-        GL11.glRotatef(90.0F, 1.0F, 0.0F, 0.0F);
-        GL11.glRotatef(90.0F, 0.0F, 0.0F, 1.0F);
+        GL11.glRotatef(180.0F, 1.0F, 0.0F, 0.0F);
+        //GL11.glRotatef(90.0F, 0.0F, 0.0F, 1.0F);
 
-
-        // Make a quad (defined higher up)
-        renderQuad(2.5F, 1.0F);
+        renderQuadCenteredFromTexture(
+            mc,
+            new ResourceLocation("opentc4:textures/models/item/scanscreen.png"),
+            2.8F,
+            1.0F, 1.0F, 1.0F,
+            190 + (int) (MathHelper.sin(player.ticksExisted + mc.theWorld.rand.nextFloat() * 2) * 10.0F + 10.0F),
+            GL11.GL_ONE_MINUS_SRC_ALPHA
+        );
 
         GL11.glPopMatrix();
         GL11.glPopMatrix();
