659eb9085ca160a005d7edc4670ece9ef257d8b8
wrldmap <123031534+wrldmap@users.noreply.github.com>
2025-07-14 16:11:54 -0500

    a basic gui

--- src/main/java/com/opentc4/common/items/ItemThaumonomicon.java
+++ src/main/java/com/opentc4/common/items/ItemThaumonomicon.java
@@ -1,21 +1,39 @@
 package com.opentc4.common.items;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
+import com.cleanroommc.modularui.api.IGuiHolder;
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.factory.GuiData;
+import com.cleanroommc.modularui.factory.GuiFactories;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.screen.UISettings;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
 import com.opentc4.OpenTC4;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemThaumonomicon extends Item {
+public class ItemThaumonomicon extends Item implements IGuiHolder<GuiData> {
 
     public ItemThaumonomicon() {
         setMaxStackSize(1);
         setMaxDamage(0);
         setCreativeTab(OpenTC4.tabOpenTC4);
+    }
+
+    public ModularPanel buildUI(GuiData guiData, PanelSyncManager guiSyncManager, UISettings settings) {
+        settings.getNEISettings()
+            .disableNEI();
+        return new ModularPanel("opentc4:itemguitest").child(
+            IKey.str("Hello")
+                .asWidget()
+                .center());
     }
 
     @Override
@@ -26,5 +44,14 @@ public class ItemThaumonomicon extends Item {
 
     public EnumRarity getRarity(ItemStack stack) {
         return EnumRarity.uncommon;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        if (!world.isRemote) {
+            GuiFactories.item()
+                .open(player);
+        }
+        return super.onItemRightClick(stack, world, player);
     }
 }
